bef7ea4b0bca12e3e3a7a49dda7eda527874bc43
Sebastian Gonzalez <seba.gnh@gmail.com>
2022-03-25 16:06:38 -0300

    Se actualizó la manera de mostrar resultados

--- src/Partido.java
+++ src/Partido.java
@@ -4,14 +4,26 @@ import java.util.Scanner;
 public class Partido {
     
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);  
-        Torneo t = new Torneo(2);
+        Scanner sc = new Scanner(System.in); 
         
+        
+
         //System.out.println("Bienvenido Ingrese el nombre del torneo");
         //String nombreTorneo = sc.next();
         String nombreTorneo = "Gran Slam";
         
-        for (int i = 0; i < 2; i++) {
+        //System.out.println("Indique la cantidad de sets");
+        //int cantSets = sc.nextInt();
+        int cantSets = 5;
+
+        int cantJugadores = 2;
+
+        Torneo t = new Torneo(nombreTorneo,cantSets,cantJugadores);
+        
+        
+        
+        
+        for (int i = 0; i < cantJugadores; i++) {
             //System.out.println("Ingrese el nombre del jugador "+ (i+1));
             //String nombre = sc.next();
             String nombre = "Jugador "+ (i+1);
@@ -26,16 +38,15 @@ public class Partido {
         }
         
         
-        //System.out.println("Indique la cantidad de sets");
-        //int cantSets = sc.nextInt();
-        int cantSets = 3;
-        System.out.println(t.getGanadorDelPunto(cantSets));
+        
+        //System.out.println(t.getGanadorDelPunto(cantSets));
+        System.out.println(t.getGanadorDelPunto());
         
         
         
         
         
-        
+        sc.close();
     }
     
 
--- src/Torneo.java
+++ src/Torneo.java
@@ -44,16 +44,6 @@ public class Torneo {
     
     
     
-
-
-
-    //---------------------------------------------------------------------------------------------------------------
-    
-
-    public Torneo(int cantJugadores) {        
-        this.jugadores = new Jugador[cantJugadores];
-    }
-    
     public void agregarJugador(Jugador j){
         for (int i = 0; i < jugadores.length; i++) {
             if (jugadores[i] == null) {
@@ -62,9 +52,18 @@ public class Torneo {
             }
         }
     }
+
+
+    //---------------------------------------------------------------------------------------------------------------
+    
+
+/*   public Torneo(int cantJugadores) {        
+        this.jugadores = new Jugador[cantJugadores];
+    } */
     
     
-    public String getGanadorDelPunto(int sets){  
+    
+    public String getGanadorDelPunto(){  
         int game0 = 0;
         int game1 = 0;
         
@@ -73,6 +72,7 @@ public class Torneo {
         
         int set = 0;
         String nombreJugador = "";
+        String ganadorTorneo = "";
         
 
         do {
@@ -83,73 +83,76 @@ public class Torneo {
 
             int puntosJugador0 = 0;
             int puntosJugador1 = 0;
-            String puntos0 = " 0";
-            String puntos1= " 0";
+            String puntos0 = "  0";
+            String puntos1 = " 0 ";
             
             int posibilidad0 = 0;
             int posibilidad1 = 0;
         
             do {
-                posibilidad0 =jugadores[0].getPosibilidadDeGanar();
-                posibilidad1 =jugadores[1].getPosibilidadDeGanar();
+                posibilidad0 = jugadores[0].getPosibilidadDeGanar();
+                posibilidad1 = jugadores[1].getPosibilidadDeGanar();
 
 
                 if (posibilidad0 > posibilidad1) {  
                     puntosJugador0 ++;
                     switch (puntosJugador0) {
-                        case 1: puntos0 = "15" ; break;
-                        case 2: puntos0 = "30"; break;
-                        case 3: puntos0 = "40"; break;
-                        case 5: puntos0 = "WD"; game0 ++; break;
+                        case 1: puntos0 = " 15" ; break;
+                        case 2: puntos0 = " 30"; break;
+                        case 3: puntos0 = " 40"; break;
+                        case 5: puntos0 = "Win"; game0 ++; break;
                     }
                     if (puntosJugador0 == 4) {                    
-                        if (puntos1.equals("AD")) {
+                        if (puntos1.equals(" AD")) {
                             puntosJugador1 = 3;
                             puntosJugador0 = 3;
-                            puntos0 = "40";
-                            puntos1 = "40";
+                            puntos0 = " 40";
+                            puntos1 = "40 ";
                         }else{
-                            puntos0 = "AD";
+                            puntos0 = " AD";
                         }   
                     }
                 }else if (posibilidad0 < posibilidad1) {  
                     puntosJugador1 ++;
                     switch (puntosJugador1) {
-                        case 1: puntos1 = "15"; break;
-                        case 2: puntos1 = "30"; break;
-                        case 3: puntos1 = "40"; break;
-                        case 5: puntos1 = "WD";game1 ++; break;
+                        case 1: puntos1 = "15 "; break;
+                        case 2: puntos1 = "30 "; break;
+                        case 3: puntos1 = "40 "; break;
+                        case 5: puntos1 = "Win";game1 ++; break;
                     }
                     if (puntosJugador1 == 4) {
-                        if (puntos0.equals("AD")) {
+                        if (puntos0.equals("AD ")) {
                             puntosJugador1 = 3;
                             puntosJugador0 = 3;
-                            puntos0 = "40";
-                            puntos1 = "40";
+                            puntos0 = " 40";
+                            puntos1 = "40 ";
                         }else{
-                            puntos1 = "AD";  
+                            puntos1 = "AD ";  
                         }   
                     }
                 }
                 resultados += "\n"+puntos0 +"  -  "+ puntos1;
             } while (puntosJugador0 <5 && puntosJugador1 <5 );
-            resultados += "\n---------\nResultado\n"+ game0 + "  -  "+game1+" \n---------\n";
+            resultados += "\n---------\nResultado\n "+ game0 + "  -  "+game1+" \n---------\n";
             set ++;
             
             // cuando alguno de los 2 llega al sets, siempre con una ventaja de 2 juegos ganados
             
             
-            if ( game0 == sets || game1 == sets ) {
+            if ( game0 == this.cantSets || game1 == this.cantSets ) {
                 if (game0 > game1) {
-                    
+                    ganadorTorneo = jugadores[0].getNombreJugador();
+                }else{
+                    ganadorTorneo = jugadores[1].getNombreJugador();
                 }
                 
                 bandera = true;
-            }
+            } 
             
             
-        } while (!bandera);    
-        return resultados;
+        } while (!bandera); 
+        
+        return resultados +"\nEl ganador del torneo es: "+ ganadorTorneo;
     }
     
     
