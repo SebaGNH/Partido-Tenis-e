9c6c3057106e1cfba4b66fb4e944290fd6398360
Sebastian Gonzalez <seba.gnh@gmail.com>
2022-03-25 15:11:42 -0300

    Actualizaré el main

--- src/Partido.java
+++ src/Partido.java
@@ -7,27 +7,28 @@ public class Partido {
         Scanner sc = new Scanner(System.in);  
         Torneo t = new Torneo(2);
         
-        System.out.println("Bienvenido Ingrese el nombre del torneo");
-        String nombreTorneo = sc.next();
+        //System.out.println("Bienvenido Ingrese el nombre del torneo");
+        //String nombreTorneo = sc.next();
+        String nombreTorneo = "Gran Slam";
         
         for (int i = 0; i < 2; i++) {
-            System.out.println("Ingrese el nombre del jugador "+ (i+1));
-            String nombre = sc.next();
-            //String nombre = "Jugador "+ (i+1);
+            //System.out.println("Ingrese el nombre del jugador "+ (i+1));
+            //String nombre = sc.next();
+            String nombre = "Jugador "+ (i+1);
             
             
-            System.out.println("Indique la probabilidad que tiene de ganar el jugador "+ nombre);
-            int probabilidad = sc.nextInt();
-            //int probabilidad = 50 ;
+            //System.out.println("Indique la probabilidad que tiene de ganar el jugador "+ nombre);
+            //int probabilidad = sc.nextInt();
+            int probabilidad = 50 ;
             
             Jugador j = new Jugador(nombre, probabilidad);
             t.agregarJugador(j);            
         }
         
         
-        System.out.println("Indique la cantidad de sets");
-        int cantSets = sc.nextInt();
-        //int cantSets = 3;
+        //System.out.println("Indique la cantidad de sets");
+        //int cantSets = sc.nextInt();
+        int cantSets = 3;
         System.out.println(t.getGanadorDelPunto(cantSets));
         
         
